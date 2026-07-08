949a1d1392a3b3170de662ca570ec02621002d36
sylvestre konan <sylvestrekonan99@gmail.com>
2026-07-08 11:53:26 +0000

    feat(api): install Laravel API and Sanctum

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -15,4 +16,9 @@ class Category extends Model
         'description',
         'is_active'
     ];
+
+    public function products(): HasMany 
+    {
+        return $this->hasMany(Product::class);
+    }
 }
