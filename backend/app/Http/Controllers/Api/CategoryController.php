e7e081a30081db3910d3a8af807fa471572ce161
sylvestre konan <sylvestrekonan99@gmail.com>
2026-07-27 16:43:45 +0000

    feat(category): complete category CRUD with validation and safe deletion

--- backend/app/Http/Controllers/Api/CategoryController.php
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
 use Illuminate\Http\Request;
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
 
+// cette class permet de creer les operations  CRUD de notre class category 
 class CategoryController extends Controller
 {
     /**
@@ -22,32 +26,44 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request): JsonResponse
     {
-        //
+        $category = Category::create($request->validated());
+
+        return response()->json($category, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category): JsonResponse
     {
-        //
+        return response()->json($category);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateCategoryRequest $request, Category $category): JsonResponse
     {
-        //
+        $category->update($request->validated());
+
+        return response()->json($category);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category): JsonResponse
     {
-        //
+        if ($category->products()->exists()){
+           return response()->json([
+            'message' => 'Impossible de supprimer cette categorie car elle contient encore des produits.'
+            ], 409);
+        } else {
+            $category->delete();
+            return response()->json(null, 204);
+        }
+
     }
 }
